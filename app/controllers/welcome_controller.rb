d39adf68fed9828b8c66a8543e88a7026deed4d2
Julie <jevans6363@gmail.com>
2017-07-24 20:00:45 -0400

    Finished up wunderground API, added styling, and added a previous cities model.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
-  def index
-  end
 
   def test
     response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/AZ/Phoenix.json")
@@ -13,5 +11,51 @@ class WelcomeController < ApplicationController
     @forecast_link = response['current_observation']['forecast_url']
     @real_feel = response['current_observation']['feelslike_f']
     @temp_f = response['current_observation']["temp_f"]
+  end
+
+
+  def index
+
+    # Creates an array of states that our user can choose from on our index page
+    @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
+
+    location_exists = false
+    Location.all.each do |location|
+      if location.city == params[:city] && location.state == params[:state]
+        location_exists = true
+      end
     end
+    if location_exists == false   # !location_exists
+      Location.create(city: params[:city], state: params[:state])
+    end
+    
+    @locations = Location.order(:city)
+
+    # removes spaces from the 2-word city names and replaces the space with an underscore 
+    if params[:city] != nil
+        params[:city].gsub!(" ", "_")
+    end
+
+  #checks that the state and city params are not empty before calling the API
+    if params[:state] != "" && params[:city] != "" && params[:state] != nil && params[:city] != nil
+      results = HTTParty.get("http://api.wunderground.com/api/#{Figaro.env.wunderground_api_key}/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
+    
+
+      # if no error is returned from the call, we fill our instants variables with the result of the call  
+      if results['response']['error'] == nil || results['error'] ==""   
+          @city = results['location']['city']
+          @state = results['location']['state']
+          @temp_f = results['current_observation']['temp_f']
+          @temp_c = results['current_observation']['temp_c']
+          @weather_icon = results['current_observation']['icon_url']
+          @weather_words = results['current_observation']['weather']
+          @forecast_link = results['current_observation']['forecast_url']
+          @real_feel_f = results['current_observation']['feelslike_f']
+          @real_feel_c = results['current_observation']['feelslike_c']
+       else
+          # if there is an error, we report it to our user via the @error variable  
+      @error = results['response']['error']['description']
+       end    
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'test' => 'welcome#test'
   get 'index' => 'welcome#index'
+  post 'index' => 'welcome#index' #pushing information rather than getting it
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
